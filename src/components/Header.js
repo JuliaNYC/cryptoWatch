e4b3dd7c73568f0665b371b8928873cd6411ddb0
Julia Siwak <JuliaNYC@users.noreply.github.com>
2018-05-15 23:26:32 +0200

    add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,26 @@ import { View, Text} from "react-native";
 const Header = () => {
     return (
         <View>
-            <Text>
-                <Text >Crypto Watch</Text></Text>
+            <Text style={styles.header}>
+                <Text style={styles.title}>Crypto Watch</Text>
+            </Text>
         </View>
     )
 }
+
+const styles = {
+    header: {
+        color: "black",
+        textAlign: "center",
+        width: 300,
+        height: 60,
+        marginTop: 50
+    },
+    title: {
+        fontSize: 22,
+        fontFamily: 'Iowan Old Style'
+    }
+}
+
 
 export default Header;
